9f8b595bb91ad77f0ff1b3465a4dfd1e0a858e8f
Abdmalique hadge <yasin80000@hotmail.com>
2020-07-10 23:36:12 -0500

    added functiion to check if there is a winner

--- game.py
+++ game.py
@@ -10,10 +10,37 @@ def display_board(board):
     print('   |   |')
     print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
     print('   |   |')
+   
     
+# takes input from user and assigns as marker   
+
+def player_input():
+    marker = ''
     
+    while not (marker == 'X' or marker == 'O'):
+        marker = input('Player 1: Do you want to be X or O? ').upper()
+
+    if marker == 'X':
+        return ('X', 'O')
+    else:
+        return ('O', 'X')
     
+# ----------------------------------------------------------------
+# it takes board list object, marker and position and assigns to the board
+
+def place_marker(board, marker, position):
+    board[position] = marker
+
+
+# check if there is a winner
+def win_check(board,mark):
     
-    
-test_board = ['#','X','O','X','O','X','O','X','O','X']
-display_board(test_board)
+    return ((board[7] == mark and board[8] == mark and board[9] == mark) or # across the top
+    (board[4] == mark and board[5] == mark and board[6] == mark) or # across the middle
+    (board[1] == mark and board[2] == mark and board[3] == mark) or # across the bottom
+    (board[7] == mark and board[4] == mark and board[1] == mark) or # down the middle
+    (board[8] == mark and board[5] == mark and board[2] == mark) or # down the middle
+    (board[9] == mark and board[6] == mark and board[3] == mark) or # down the right side
+    (board[7] == mark and board[5] == mark and board[3] == mark) or # diagonal
+    (board[9] == mark and board[5] == mark and board[1] == mark)) # diagonal
+
